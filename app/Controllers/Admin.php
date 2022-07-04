60ec4b443a59d2dca0304f639f14a519fa3c17c6
Luqman Firmansyah <luqm4n.firm4n@gmail.com>
2022-07-05 02:44:46 +0700

    Add Export to Excel Function

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\User;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class Admin extends BaseController
 {
@@ -23,8 +25,55 @@ class Admin extends BaseController
     public function exportToExcel()
     {
         $model = new User();
-        $query = $model->select('id, name, email, sekolah, nisn, wa, kota, provinsi, image, bukti_nisn, bukti_bayar, role_id');
-        $data = $query->find();
-        return view('export_excel', [$data]);
+        $role = $model->builder('user_roles')->select()->get()->getResultArray();
+
+        foreach ($role as $value) {
+            $data[$value['name']] = $model->select('id, name, email, sekolah, nisn, wa, kota, provinsi, image, bukti_nisn, bukti_bayar')
+                ->where('role_id', $value['id'])->find();
+
+        }
+        $header = [
+            'No', 'Nama', 'Email', 'Sekolah', 'Nisn', 'No. Wa', 'Kota', 'Provinsi', 'Image', 'Bukti NISN', 'Bukti Bayar'
+        ];
+        
+        $spreadsheet = new Spreadsheet();
+        $spreadsheet->getProperties()->setTitle('Rekap Peserta OMITS15th');
+
+        $sheetIndex = 0;
+        foreach ($data as $key => $value) {
+            $sheet = $spreadsheet->setActiveSheetIndex($sheetIndex);
+            $spreadsheet->getActiveSheet($sheetIndex)->setTitle($key);
+            foreach ($header as $key => $hd) {
+                $sheet->setCellValue([$key+1, 1], $hd);
+            }
+
+            $rowIndex = 2;
+            foreach ($value as $rowData) {
+                $colomnIndex = 1;
+                foreach ($rowData as $cellData) {
+                    if ($colomnIndex==1) {
+                        $sheet->setCellValue([$colomnIndex, $rowIndex], $rowIndex-1);
+                    } else {
+                        $sheet->setCellValue([$colomnIndex, $rowIndex], $cellData);
+                    }
+                    $colomnIndex++;
+                }
+                $rowIndex++;
+            }
+            
+            $spreadsheet->createSheet();
+            $sheetIndex++;
+        }
+
+        $spreadsheet->setActiveSheetIndex(0);
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="Rekap Peserta OMITS15th.xlsx"');
+        header('Cache-Control: max-age=0');
+        
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save('php://output');
+
+        exit;
+
     }
 }
